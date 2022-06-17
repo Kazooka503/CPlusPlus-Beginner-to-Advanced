960fadcad45b081bca99860365f8c19455852acd
Austin Krzciok <austinkrzciok@novuslabs.com>
2022-06-17 11:12:54 -0700

    Lecture 2 -
    Covering comments in cpp files

---
+++ Comments/main.cpp
@@ -0,0 +1,20 @@
+//
+// Created by austinkrz on 6/17/22.
+//
+
+//One line comment
+
+/*
+    This is a block comment that englobes multiple
+    lines of text
+ */
+
+// This brings in the iostream library
+#include <iostream>
+
+int main(){
+    // this is going to bring hello world to the console
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+    // Program ends here
+}
