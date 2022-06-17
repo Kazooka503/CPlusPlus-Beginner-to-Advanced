e3d8feefd79e5087a387721cec2005dce3990d92
Austin Krzciok <austinkrzciok@novuslabs.com>
2022-06-17 10:53:10 -0700

    Lecture 1 -
    Covering main function, IO stream library, and consoling out strings to the terminal correctly.

---
+++ FirstCppProgram/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by austinkrz on 6/17/22.
+//
+#include <iostream>
+
+int main(){
+    std::cout << "Number1" << std::endl;
+    std::cout << "Number2" << std::endl;
+    std::cout << "Number3" << std::endl;
+
+    return 0;
+}
